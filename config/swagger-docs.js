47edde939d13e7a5237facc9c53186793bfddba7
GwariRhythm <rhythm.gwari@omlogic.com>
2024-11-05 11:27:40 +0530

    Updated swagger docs

--- config/swagger-docs.js
+++ config/swagger-docs.js
@@ -11,7 +11,7 @@ const swaggerOptions = {
     },
     servers: [
       {
-        url: 'http://localhost:8000/api', // Replace with your server URL
+        url: 'https://zugo-test.onrender.com/api', // Replace with your server URL
       },
     ],
   },
